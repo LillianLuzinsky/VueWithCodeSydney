c2b8a8c97285225858443b897fa70d6092077b76
Lillian Luzinsky <eyeris@tpg.com.au>
2020-06-11 09:11:18 +1000

    removed computed properties

--- js/main.js
+++ js/main.js
@@ -3,22 +3,19 @@ var app = new Vue({
     data: {
         brand: 'Vue Mastery',
         product: 'Socks',
-        selectedVariant: 0,
+        image: './assets/vmSocks-green-onWhite.jpg',
+        inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
                 variantImage: './assets/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10,
-                variantOnSale: true
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
                 variantImage: './assets/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0,
-                variantOnSale: false
             }
         ],
         cart: 0   
@@ -31,19 +28,8 @@ var app = new Vue({
             if (this.cart === 0) return
             this.cart -= 1
           },
-        updateProduct(index) {
-            this.selectedVariant = index
+        updateProduct(variantImage) {
+            this.image = variantImage
         }
     },
-    computed: {
-        onSale(){
-            return this.brand + ' ' + this.product
-        },
-        image(){
-            return this.variants[this.selectedVariant].variantImage
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-    }
 })
