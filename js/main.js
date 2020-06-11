bb1e85486df0b251aeacd6e9ab36005ece137917
Lillian Luzinsky <eyeris@tpg.com.au>
2020-06-11 10:52:49 +1000

    created product component

--- js/main.js
+++ js/main.js
@@ -1,60 +1,59 @@
 Vue.component('product', {
     template: `
-    <div class="product-image">
-    <img v-bind:src="image" alt="Vue socks">
-  </div>
+    <div id="product">
+        <div class="product-image">
+            <img v-bind:src="image" alt="Vue socks">
+        </div>
 
-  <div class="product-info">
-    <h1>{{ product }}</h1>
-    <p v-if="inStock">In Stock</p>
-    <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
-    <!-- <p>Shipping: {{ shipping }}</p> -->
+        <div class="product-info">
+            <h1>{{ product }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
 
-    <ul>
-      <li v-for="detail in details">{{ detail }}</li>
-    </ul>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
 
-    <div v-for="(variant, index) in variants" 
-        :key="variant.variantId"
-        class="color-box"
-        :style="{ background: variant.variantColor }"
-        @mouseover="updateProduct(index)">
+            <div v-for="(variant, index) in variants" 
+                :key="variant.variantId"
+                class="color-box"
+                :style="{ background: variant.variantColor }"
+                @mouseover="updateProduct(index)">
+            </div>
+                
+            <button v-on:click="addToCart" 
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }">Add To Cart</button>
+            <button @click="remove">Remove</button>
+
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+        </div>
     </div>
-        
-    <button v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }">Add To Cart</button>
-    <button @click="remove">Remove</button>
-
-    <div class="cart">
-      <p>Cart({{ cart }})</p>
-    </div>
-  </div>
-    `
-})
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant: 0,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: './assets/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: './assets/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0
-            }
-        ],
-        cart: 0   
+    `,
+    data() {
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: './assets/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0   
+        }
     },
     methods: {
         addToCart () {
@@ -80,4 +79,9 @@ var app = new Vue({
             return this.variants[this.selectedVariant].variantQuantity
         }
     }
+})
+
+var app = new Vue({
+    el: '#app',
+    
 })
